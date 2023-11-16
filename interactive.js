9dc59d9aef92495c58d1e5efcd10117966bb8960
Ram Shreyas Naik Sabavat <ramsabavat2012@gmail.com>
2023-11-16 15:59:06 -0800

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
-            // Array of facts about the Nile and Ancient Egypt
-            const facts = [
-                "The Nile provided fertile soil for agriculture.",
-                "It served as a natural transportation route.",
-                // Add more facts here
-            ];
+    // Fetch data from the JSON file
+    $.getJSON("facts.json", function (data) {
+        // Get the ul element where we will add the facts
+        const factList = $("#factList");
 
-            // Get the ul element where we will add the facts
-            const factList = $("#factList");
-
-            // Populate the list with facts using jQuery
-            $.each(facts, function (index, fact) {
-                const listItem = $("<li>").text(fact);
-                factList.append(listItem);
-            });
+        // Populate the list with facts
+        data.facts.forEach((fact) => {
+            const listItem = $("<li>").text(fact);
+            factList.append(listItem);
         });
+    });
+});
