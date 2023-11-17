420f5d5f25342972c75bbb4222be8be03145dea2
Ram Shreyas Naik Sabavat <ramsabavat2012@gmail.com>
2023-11-16 21:08:16 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,16 @@
+// Dynamically generate and display quiz questions
+const quizContainer = $("#quizContainer");
 
+$.getJSON("quiz.json", function (data) {
+    data.quiz.forEach((questionObj, index) => {
+        const questionDiv = $("<div>").addClass("question");
+        const questionText = $("<p>").text(`${index + 1}. ${questionObj.question}`);
+        questionDiv.append(questionText);
+
+        // Generate and append options (radio buttons, etc.)
+        // Add event listeners to check user answers
+        // Update the UI based on correct/incorrect answers
+        
+        quizContainer.append(questionDiv);
+    });
+});
